202406639c29676a747a238ce07ae5b0e10c5a7a
cm-takahashi-yudai <takahashi.yudai@classmethod.jp>
2019-04-23 14:55:45 +0900

    Update create_csv.py

--- src/glue_jobs/create_csv.py
+++ src/glue_jobs/create_csv.py
@@ -1,31 +1,49 @@
 import sys
-from awsglue.transforms import ApplyMapping
+from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
-# @params: [JOB_NAME]
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 
+# Initalize
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
-# @type: DataSource
-# @args: [database = "aws-glue-dynamodb-export-csv-dev-glue-database", table_name = "aws-glue-dynamodb-export-csv-dev-glue-table", transformation_ctx = "datasource0"]
-# @return: datasource0
-# @inputs: []
-datasource0 = glueContext.create_dynamic_frame.from_catalog(database="aws-glue-dynamodb-export-csv-dev-glue-database", table_name="aws-glue-dynamodb-export-csv-dev-glue-table", transformation_ctx="datasource0")
-# @type: ApplyMapping
-# @args: [mapping = [("sample_col_1", "string", "sample_col_1", "string"), ("sample_col_2", "string", "sample_col_2", "string"), ("sample_col_3", "long", "sample_col_3", "long"), ("sample_id", "string", "sample_id", "string")], transformation_ctx = "applymapping1"]
-# @return: applymapping1
-# @inputs: [frame = datasource0]
-applymapping1 = ApplyMapping.apply(frame=datasource0, mappings=[("sample_col_1", "string", "sample_col_1", "string"), ("sample_col_2", "string", "sample_col_2", "string"), ("sample_col_3", "long", "sample_col_3", "long"), ("sample_id", "string", "sample_id", "string")], transformation_ctx="applymapping1")
-# @type: DataSink
-# @args: [connection_type = "s3", connection_options = {"path": "s3://aws-glue-dynamodb-export-csv-dev-result"}, format = "csv", transformation_ctx = "datasink2"]
-# @return: datasink2
-# @inputs: [frame = applymapping1]
-datasink2 = glueContext.write_dynamic_frame.from_options(frame=applymapping1, connection_type="s3", connection_options={"path": "s3://aws-glue-dynamodb-export-csv-dev-result"}, format="csv", transformation_ctx="datasink2")
+
+# DataSource
+datasource = glueContext.create_dynamic_frame.from_catalog(
+    database="aws-glue-dynamodb-export-csv-dev-glue-database",
+    table_name="aws-glue-dynamodb-export-csv-dev-glue-table",
+    transformation_ctx="datasource",
+    push_down_predicate="create_date='2019-04-22'"
+)
+
+# Transform
+applymapping = ApplyMapping.apply(
+    frame=datasource,
+    mappings=[
+        ("sample_col_1", "string", "sample_col_1", "string"),
+        ("sample_col_2", "string", "sample_col_2", "string"),
+        ("sample_col_3", "long", "sample_col_3", "long"),
+        ("sample_id", "string", "sample_id", "string")
+    ],
+    transformation_ctx="applymapping"
+)
+
+# DataSink
+datasink = glueContext.write_dynamic_frame.from_options(
+    frame=applymapping,
+    connection_type="s3",
+    connection_options={
+        "path": "s3://aws-glue-dynamodb-export-csv-dev-result"
+    },
+    format="csv",
+    transformation_ctx="datasink"
+)
+
+# Terminate
 job.commit()
